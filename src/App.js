0307fb7a61987510ecbd73ea97b1f5f508160ecf
xayi <abdigaraad506@gmail.com>
2020-08-20 17:01:53 +0300

    Create-Product-Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@
 
 import React from 'react';
 import './index.css'
-function App() {
+import date from './date.json'
+import Products from './components/Products';
+
+
+
+class App extends React.Component{
+
+  constructor() {
+    super();
+    this.state = {
+      products: date.products,
+      size: "",
+      sort: "",
+    };
+  }
+
+  render() {
   return (
     <div  className="grid-container">
       <header>
         <a href="/">Somali shoping cart</a>
       </header>
       <main>
-        products
+      <div className="content">
+      <div className="main">
+        <Products products={this.state.products}></Products>
+      </div>
+      <div className="sidebar">
+      curt 
+      </div>
+
+      </div>
       </main>
       <footer>
         all right reserved devloper by xayi garaad
@@ -18,6 +42,7 @@ function App() {
 
    
   );
+  }
 }
 
 export default App;
